06bbc3f0cf781bc7aabe7ba057304b361c2fc523
Padakk <padakk3166@gmail.com>
2023-12-02 09:10:28 +0900

    23-12-02 게시글 상세페이지

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,8 +4,10 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
@@ -26,4 +28,22 @@ public class BoardController {
 
         return "";
     }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardList";
+    }
+
+    @GetMapping("/board/view")  // localhost:8080/board/view?id=1
+    public String boardView(Model model, @RequestParam(name = "id") Integer id) {
+
+
+        model.addAttribute("board", boardService.boardview(id));
+        return "boardview";
+    }
+
+
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,14 +5,30 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
     @Autowired  //객체 생성없이 사용가능하게 만들어주는 어노테이션
     private BoardRepository boardRepository;
 
+
+    //글 작성 처리
     public void write(Board board) {
 
         boardRepository.save(board);
+    }
+
+    //게시글 리스트 처리
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
+    }
+
+    //특정 게시글 불러오기
+    public Board boardview(Integer id) {
+
+        return boardRepository.findById(id).get();
     }
 }
